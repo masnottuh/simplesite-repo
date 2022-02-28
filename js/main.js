29b66501fd74bb76aca6cf5e1719882f2d32b301
masnottuh <samhutton543@gmail.com>
2022-02-27 19:58:14 -0800

    sh

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// 1. Declare Global Variables
+// 2. Declare Constructor functions - name should be capitalized!
+// 3. Declare prototype methods
+// 4. Declare regular functions
+// 5. Add event listeners
+// 6. Call functions
